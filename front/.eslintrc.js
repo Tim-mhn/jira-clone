5fd0ae40b5337820769424e043ac2f4b67e31e3b
Timothy Meehan <tim.meehan097@gmail.com>
2022-12-12 16:28:05 +0700

    added airbnb eslint config

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -10,42 +10,26 @@ module.exports = {
     sourceType: 'module',
   },
   settings: {
-    // 'import/parsers': {
-    //   '@typescript-eslint/parser': ['.ts'],
-    // },
-    // 'import/resolver': {
-    //   typescript: {},
-    //   node: {
-    //     extensions: ['.js', '.ts'],
-    //   },
-    // },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
   },
-  plugins: ['@typescript-eslint', 'unused-imports', 'import'],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
   rules: {
     'import/prefer-default-export': 'off',
     'no-console': 'off',
     'class-methods-use-this': 'off',
-    'no-underscore-dangle': 'off',
-    'no-useless-constructor': 'off',
-    'no-empty-function': 'off',
+    //     'no-underscore-dangle': 'off',
+    //     'no-useless-constructor': 'off',
+    //     'no-empty-function': 'off',
     'unused-imports/no-unused-imports': 'error',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        ts: 'never',
-      },
-    ],
-    'scss.lint.unknownAtRules': 'off',
-    'lines-between-class-members': 'off',
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-    'prefer-destructuring': 'off',
-    unknownAtRules: 'off',
-    'import/no-cycle': 'off',
-    'no-return-assign': ['error', 'except-parens'],
-    radix: 'off',
+    'import/no-unresolved': 'error',
     'no-unused-expressions': [
       'error',
       {
