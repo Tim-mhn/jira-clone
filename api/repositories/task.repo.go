cb960491453a45e3883e7a5868ba6d3610f93524
Timothy Meehan <tim.meehan097@gmail.com>
2022-12-30 18:29:33 +0100

    factorized logic to fetch one or multiple tasks and handle potential null assignee ID

--- api/repositories/task.repo.go
+++ api/repositories/task.repo.go
@@ -23,104 +23,83 @@ func NewTaskRepository(um *UserRepository, pm *ProjectRepository, conn *sql.DB) 
 	return &taskRepo
 }
 
-func spreadErrorMessage(contextErrorMessage string, sourceError error) error {
+func addContextToError(contextErrorMessage string, sourceError error) error {
 	return fmt.Errorf(`%s Details: %s `, contextErrorMessage, sourceError.Error())
 }
+
 func (taskRepo TaskRepository) GetProjectTasks(projectID string) ([]models.Task, error) {
 
 	BASE_ERROR_MESSAGE := fmt.Sprintf(`error  when fetching list of tasks of project %s`, projectID)
 
-	getProjectTasksRequest := fmt.Sprintf(`
-	SELECT task.id as task_id, 
-	task.title as task_title,
-	task.points as task_points,
-	task.description as task_description,
-	COALESCE(task.status, 0) as task_status,
-	COALESCE(task_status.label, '') as task_status_label,
-	assignee_id, 
-	COALESCE("user".name, '') as user_name, 
-	COALESCE("user".email, '') as user_email 
-	from task 
-	LEFT JOIN "user" ON assignee_id="user".id
-	LEFT JOIN task_status ON task_status.id=task.status
-	WHERE task.project_id='%s' 
-	`, projectID)
+	tasksOfProjectQuery := buildGetTasksOfProjectQuery(projectID)
 
-	var assigneeIdBytes []byte
-
-	tasks := []models.Task{}
-
-	rows, err := taskRepo.conn.Query(getProjectTasksRequest)
+	rows, err := taskRepo.conn.Query(tasksOfProjectQuery)
 
 	if err != nil {
-		return []models.Task{}, spreadErrorMessage(BASE_ERROR_MESSAGE, err)
+		return []models.Task{}, addContextToError(BASE_ERROR_MESSAGE, err)
 	}
 
 	defer rows.Close()
 
+	tasks := []models.Task{}
+
 	for rows.Next() {
 
-		var task models.Task
-		var assignee models.User
-		err := rows.Scan(
-			&task.Id, &task.Title, &task.Points, &task.Description,
-			&task.Status.Id, &task.Status.Label,
-			&assigneeIdBytes, &assignee.Name, &assignee.Email)
+		task, err := getTaskDataFromRow(rows)
 
 		if err != nil {
-			return []models.Task{}, spreadErrorMessage(BASE_ERROR_MESSAGE, err)
+			return []models.Task{}, addContextToError(BASE_ERROR_MESSAGE, err)
 		}
 
-		task.Assignee = assignee
-		assigneeIdString := string(assigneeIdBytes)
-		task.Assignee.Id = assigneeIdString
-
 		tasks = append(tasks, task)
+
 	}
 
 	return tasks, nil
 }
 
-func (taskRepo *TaskRepository) GetTaskById(taskID string) (models.Task, error) {
-	getTaskRequest := fmt.Sprintf(`
-	SELECT task.id as task_id, 
-	task.title as task_title,
-	task.points as task_points,
-	task.description as task_description,
-	task.status as task_status,
-	task_status.label as task_status_label,
-	"user".id as user_id, 
-	"user".name as user_name, 
-	user.email as user_email 
-	from task 
-	LEFT JOIN "user" ON assignee_id="user".id
-	LEFT JOIN task_status ON task_status.id=task.status
-	WHERE task.id='%s' 
-	LIMIT 1 
-	`, taskID)
+func getTaskDataFromRow(rows *sql.Rows) (models.Task, error) {
+	var task models.Task
+	var assignee models.User
+	var assigneeIdBytes []byte
 
-	rows, err := taskRepo.conn.Query(getTaskRequest)
+	err := rows.Scan(
+		&task.Id, &task.Title, &task.Points, &task.Description,
+		&task.Status.Id, &task.Status.Label,
+		&assigneeIdBytes, &assignee.Name, &assignee.Email)
 
 	if err != nil {
 		return models.Task{}, err
 	}
 
-	defer rows.Close()
+	task.Assignee = assignee
+	assigneeIdString := string(assigneeIdBytes)
+	task.Assignee.Id = assigneeIdString
 
-	var task models.Task
-	var assignee models.User
-	if rows.Next() {
-		err := rows.Scan(
-			&task.Id, &task.Title, &task.Points, &task.Description,
-			&task.Status.Id, &task.Status.Label,
-			&assignee.Id, &assignee.Name, &assignee.Email)
+	return task, nil
 
-		if err != nil {
-			return models.Task{}, err
-		}
+}
+
+func (taskRepo *TaskRepository) GetTaskById(taskID string) (models.Task, error) {
+	getTaskRequest := buildSingleTaskQuery(taskID)
+
+	rows, err := taskRepo.conn.Query(getTaskRequest)
+
+	if err != nil {
+		return models.Task{}, addContextToError(fmt.Sprintf(`error when querying task %s `, taskID), err)
 	}
 
-	task.Assignee = assignee
+	defer rows.Close()
+
+	if !rows.Next() {
+		return models.Task{}, fmt.Errorf(`no data found for task %s`, taskID)
+	}
+
+	task, err := getTaskDataFromRow(rows)
+
+	if err != nil {
+		return models.Task{}, err
+	}
 
 	return task, nil
 
@@ -192,4 +171,25 @@ func (taskRepo *TaskRepository) UpdateTask(taskID string, patchDTO dtos.PatchTas
 
 	return err
 
+}
+
+const TASK_REQUEST string = `SELECT task.id as task_id, 
+	task.title as task_title,
+	task.points as task_points,
+	task.description as task_description,
+	COALESCE(task.status, 0) as task_status,
+	COALESCE(task_status.label, '') as task_status_label,
+	assignee_id,
+	COALESCE("user".name, '') as user_name,
+	COALESCE("user".email, '') as user_email
+	from task
+	LEFT JOIN "user" ON assignee_id="user".id
+	LEFT JOIN task_status ON task_status.id=task.status`
+
+func buildGetTasksOfProjectQuery(projectID string) string {
+	return fmt.Sprintf(`%s WHERE task.project_id='%s'`, TASK_REQUEST, projectID)
+}
+
+func buildSingleTaskQuery(taskID string) string {
+	return fmt.Sprintf(`%s WHERE task.id='%s' LIMIT 1 `, TASK_REQUEST, taskID)
 }
