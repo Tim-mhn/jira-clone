e745bfcab8cf7850a88ac23e1bf27ea2dd164743
Timothy Meehan <tim.meehan097@gmail.com>
2023-01-10 18:38:50 +0100

    made points and description fields optional in NewTaskDTO

--- api/dtos/new-task.dto.go
+++ api/dtos/new-task.dto.go
@@ -1,9 +1,9 @@
 package dtos
 
 type NewTaskDTO struct {
-	Points      int    `json:"points"`
+	Points      int    `json:"points,omitempty"`
 	Title       string `json:"title"`
 	AssigneeId  string `json:"assigneeId"`
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	SprintId    string `json:"sprintId"`
 }
