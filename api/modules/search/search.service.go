70c1f0e3bd7841b94acea9724bf565adc3cd0af7
Timothy Meehan <tim.meehan097@gmail.com>
2023-02-08 19:50:01 +0100

    improved search tasks/sprints with goroutines

--- api/modules/search/search.service.go
+++ api/modules/search/search.service.go
@@ -1,5 +1,10 @@
 package search
 
+import (
+	"log"
+	"sync"
+)
+
 type SearchService struct {
 	repo       *SearchTasksRepository
 	sprintRepo *SearchSprintsRepository
@@ -14,16 +19,43 @@ func NewSearchService(repo *SearchTasksRepository, sprintRepo *SearchSprintsRepo
 
 func (service *SearchService) searchTasksOrSprintsOfUserByText(searchInput SearchInput) (SearchResults, error) {
 
-	tasks, tasksError := service.repo.SearchTasksWithMatchingContentInUserProjects(searchInput)
-	sprints, sprintsError := service.sprintRepo.SearchSprintOfUsersByName(searchInput)
+	tasksChan := make(chan []TaskInfo)
+	sprintsChan := make(chan []SprintInfo)
+	errorChan := make(chan error)
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		tasks, tasksError := service.repo.SearchTasksWithMatchingContentInUserProjects(searchInput)
+		if tasksError != nil {
+			errorChan <- tasksError
+			log.Print(tasksError)
+		}
+		tasksChan <- tasks
+		wg.Done()
+	}()
+
+	go func() {
+
+		sprints, sprintsError := service.sprintRepo.SearchSprintOfUsersByName(searchInput)
+		if sprintsError != nil {
+			errorChan <- sprintsError
+			log.Print(sprintsError)
+		}
+		sprintsChan <- sprints
+		wg.Done()
+	}()
+	tasks := <-tasksChan
+	sprints := <-sprintsChan
+	err := <-errorChan
+	errorChan <- nil
+	wg.Wait()
+
 	searchResults := SearchResults{
 		Tasks:   tasks,
 		Sprints: sprints,
-	}
-
-	var err = tasksError
-	if sprintsError != nil {
-		err = sprintsError
 	}
 
 	return searchResults, err
