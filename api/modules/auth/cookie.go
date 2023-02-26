c8fa219a788a5609563cea31a479cd22bb9ac562
Timothy Meehan <tim.meehan097@gmail.com>
2023-02-26 21:55:13 +0100

    added same site none to allow cross origin to use cookie

--- api/modules/auth/cookie.go
+++ api/modules/auth/cookie.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,17 +10,17 @@ import (
 func SetAuthCookieFromUser(user User, c *gin.Context) {
 	ss := CreateJWTSignedString(user)
 
-	c.SetCookie("Authorization", ss, 365*24*60*60, "/", "tim-jira.netlify.app", true, true)
-	// http.SetCookie(c.Writer, &http.Cookie{
-	// 	Name:     "Authorization",
-	// 	Value:    url.QueryEscape(ss),
-	// 	MaxAge:   365 * 24 * 60 * 60,
-	// 	Path:     "/",
-	// 	Domain:   "localhost",
-	// 	SameSite: http.SameSiteDefaultMode,
-	// 	Secure:   true,
-	// 	HttpOnly: true,
-	// })
+	// c.SetCookie("Authorization", ss, 365*24*60*60, "/", "tim-jira.netlify.app", true, true)
+	http.SetCookie(c.Writer, &http.Cookie{
+		Name:     "Authorization",
+		Value:    url.QueryEscape(ss),
+		MaxAge:   365 * 24 * 60 * 60,
+		Path:     "/",
+		Domain:   "tim-jira.netlify.app",
+		SameSite: http.SameSiteNoneMode,
+		Secure:   true,
+		HttpOnly: true,
+	})
 }
 func DeleteAuthCookie(c *gin.Context) {
 	cookie := &http.Cookie{
