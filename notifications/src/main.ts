d019b608b594825f12e2a33cd97a34eacabc3901
Timothy Meehan <tim.meehan097@gmail.com>
2023-03-18 21:42:42 +0100

    added client url (cors) as env variable

--- notifications/src/main.ts
+++ notifications/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://tim-jira.live',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
