5c2ebf954aa87463192f3c928ba58a09d527ef20
Timothy Meehan <tim.meehan097@gmail.com>
2023-03-18 21:37:34 +0100

    added cors origin

--- notifications/src/main.ts
+++ notifications/src/main.ts
@@ -1,12 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logger } from './shared/logger/logger.middleware';
+import { logger } from './shared/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://tim-jira.live',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(logger);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
